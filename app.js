6791e7888ac13e492fd6c06d05fdf9baee20e245
Llll0l0l <h0cker.hack1@gmail.com>
2023-06-03 17:10:15 +0400

    dynamic restaurant site generator with post and get requests

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const fs = require("fs");
+
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    res.render('index');
+});
+
+app.get("/about", (req, res) => {
+    res.render('about');
+});
+
+app.get("/confirm", (req, res) => {
+    res.render('confirm');
+});
+
+app.get("/recommend", (req, res) => {
+    res.render('recommend');
+});
+
+app.get("/restaurants", (req, res) => {
+    const filePath = path.join(__dirname, "data", "restaurants.json");
+
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+    res.render('restaurants', { numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants });
+});
+
+
+app.post("/recommend", (req, res) => {
+    const restaurant = req.body;
+    const filePath = path.join(__dirname, "data", "restaurants.json");
+
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+    storedRestaurants.push(restaurant);
+
+    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+    res.redirect("/confirm");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
