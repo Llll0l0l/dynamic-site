ffe2e6f9246ccdba5a2720a0b8042e8c9a893a9f
Llll0l0l <h0cker.hack1@gmail.com>
2023-06-07 10:44:11 +0400

    added 404, dynamic routing, incorrect filepath middleware

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 
+const uuid = require("uuid");
+
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
@@ -37,9 +39,28 @@ app.get("/restaurants", (req, res) => {
     res.render('restaurants', { numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants });
 });
 
+app.get('/restaurants/:id', (req, res) => {
+    const restaurantId = req.params.id;
+    const filePath = path.join(__dirname, "data", "restaurants.json");
+
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+    for (const restaurant of storedRestaurants) {
+        if (restaurant.id === restaurantId) {
+            return res.render('restaurant-detail', { restaurant: restaurant });
+
+        }
+    }
+
+    res.render('404');
+
+});
+
 
 app.post("/recommend", (req, res) => {
     const restaurant = req.body;
+    restaurant.id = uuid.v4();
     const filePath = path.join(__dirname, "data", "restaurants.json");
 
     const fileData = fs.readFileSync(filePath);
@@ -50,6 +71,12 @@ app.post("/recommend", (req, res) => {
     fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
 
     res.redirect("/confirm");
+});
+
+
+
+app.use(function(req, res) {
+    res.render('404');
 });
 
 app.listen(3000, () => {
